be51b2d3bd05a36df39d7813f7597d3ed1bae6d7
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-11-05 02:54:05 +0900

    Spring Swagger 문제 해결을 위해 DevTools 제거

--- src/main/java/com/example/recipeLabs/repository/UserRepository.java
+++ src/main/java/com/example/recipeLabs/repository/UserRepository.java
@@ -7,7 +7,4 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByKakaoId(Long kakaoId);
-    Boolean existsByNickname(String nickname);
-    Boolean existsByEmail(String email);
 }
