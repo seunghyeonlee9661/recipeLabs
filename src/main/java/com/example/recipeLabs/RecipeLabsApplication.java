c9dfdac6fb03fcd62ced8244017b9fc516c5543b
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-11-05 00:05:05 +0900

    Initial commit

---
+++ src/main/java/com/example/recipeLabs/RecipeLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipeLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recipeLabs/RecipeLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
