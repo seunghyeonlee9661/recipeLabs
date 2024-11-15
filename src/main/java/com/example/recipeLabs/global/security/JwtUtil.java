ef9abc1376912954e59e5abfb2aea3bad631c485
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-11-15 16:06:08 +0900

    JWT 잠시 10초로 수정

--- src/main/java/com/example/recipeLabs/global/security/JwtUtil.java
+++ src/main/java/com/example/recipeLabs/global/security/JwtUtil.java
@@ -37,7 +37,8 @@ JWT 생성, 검증을 맡은 클래스
 public class JwtUtil {
     public static final String AUTHORIZATION_HEADER = "Authorization"; // Header KEY 값
     public static final String BEARER_PREFIX = "Bearer "; // Token 식별자
-    private final long ACCESS_TOKEN_VALIDITY = 60 * 60 * 1000L; // Access Token 만료시간 : 1시간
+//    private final long ACCESS_TOKEN_VALIDITY = 60 * 60 * 1000L; // Access Token 만료시간 : 1시간
+    private final long ACCESS_TOKEN_VALIDITY = 10 * 1000L; // Access Token 만료시간 : 1시간
 
     @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
     private String secretKey;
