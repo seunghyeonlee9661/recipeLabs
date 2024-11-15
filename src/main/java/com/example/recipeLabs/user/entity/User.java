b5ad20dcd4ba6f6e018e6d67bb7a34b15a73cb86
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-11-15 17:35:45 +0900

    LazyLoading 관련 문제 해결

--- src/main/java/com/example/recipeLabs/user/entity/User.java
+++ src/main/java/com/example/recipeLabs/user/entity/User.java
@@ -74,7 +74,7 @@ public class User {
     private String emailVerificationCode;
 
     // 팔로우하는 사람들 (이 사용자가 팔로우한 사용자들)
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "follow", // 관계 테이블 이름
             joinColumns = @JoinColumn(name = "follower_id"), // 팔로우하는 사람
